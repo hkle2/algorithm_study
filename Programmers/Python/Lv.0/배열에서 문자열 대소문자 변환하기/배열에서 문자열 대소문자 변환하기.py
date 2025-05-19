a8406688ada2621bbfa2266e15a43a09e666db50
hklee <hklee2598@gmail.com>
2025-05-19 15:44:25 +0900

    Time: 0.01 ms, Memory: 9.18 MB

--- Programmers/Python/Lv.0/배열에서 문자열 대소문자 변환하기/배열에서 문자열 대소문자 변환하기.py
+++ Programmers/Python/Lv.0/배열에서 문자열 대소문자 변환하기/배열에서 문자열 대소문자 변환하기.py
@@ -1,7 +1,10 @@
+# def solution(strArr):
+#     for i, str in enumerate(strArr):
+#         if i % 2 != 0:
+#             strArr[i] = str.upper()
+#         else:
+#             strArr[i] = str.lower()
+#     return strArr
+
 def solution(strArr):
-    for i, str in enumerate(strArr):
-        if i % 2 != 0:
-            strArr[i] = str.upper()
-        else:
-            strArr[i] = str.lower()
-    return strArr
+    return [str.upper() if i % 2 else str.lower() for i, str in enumerate(strArr)]

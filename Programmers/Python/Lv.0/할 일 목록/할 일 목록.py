81c8dff8e03148b45be651ec01d00e1b1bdc95b3
hklee <hklee2598@gmail.com>
2025-05-24 20:04:42 +0900

    Time: 0.00 ms, Memory: 9.1 MB

--- Programmers/Python/Lv.0/할 일 목록/할 일 목록.py
+++ Programmers/Python/Lv.0/할 일 목록/할 일 목록.py
@@ -1,6 +1,9 @@
+# def solution(todo_list, finished):
+#     answer = []
+#     for todo, finish in zip(todo_list, finished):
+#         if not finish:
+#             answer.append(todo)
+#     return answer
+
 def solution(todo_list, finished):
-    answer = []
-    for todo, finish in zip(todo_list, finished):
-        if not finish:
-            answer.append(todo)
-    return answer
+    return [todo for todo, finish in zip(todo_list, finished) if not finish]

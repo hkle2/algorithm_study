32f39ba89aac544311953e16800049d2d28dc95a
hklee <hklee2598@gmail.com>
2025-05-24 18:07:27 +0900

    Time: 11.92 ms, Memory: 15.6 MB

--- Programmers/Python/Lv.0/조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.py
+++ Programmers/Python/Lv.0/조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.py
@@ -1,7 +1,15 @@
+# def solution(arr):
+#     for i in range(len(arr)):
+#         if arr[i] >= 50 and arr[i] % 2 == 0:
+#             arr[i] /= 2
+#         elif arr[i] < 50 and arr[i] % 2 != 0:
+#             arr[i] *= 2
+#     return arr
+
 def solution(arr):
-    for i in range(len(arr)):
-        if arr[i] >= 50 and arr[i] % 2 == 0:
-            arr[i] /= 2
-        elif arr[i] < 50 and arr[i] % 2 != 0:
-            arr[i] *= 2
+    for i, num in enumerate(arr):
+        if num >= 50 and num % 2 == 0:
+            arr[i] = num / 2
+        elif num < 50 and num % 2 != 0:
+            arr[i] = num * 2
     return arr

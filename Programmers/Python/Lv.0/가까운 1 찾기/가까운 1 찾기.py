b3c03522fddf6a0a3156932ed1cb17381bf9ea8a
hklee <hklee2598@gmail.com>
2025-05-24 20:52:08 +0900

    Time: 0.01 ms, Memory: 9.14 MB

--- Programmers/Python/Lv.0/가까운 1 찾기/가까운 1 찾기.py
+++ Programmers/Python/Lv.0/가까운 1 찾기/가까운 1 찾기.py
@@ -1,2 +1,8 @@
+# def solution(arr, idx):
+#     return next((i for i in range(idx, len(arr)) if arr[i] == 1), -1)
+
 def solution(arr, idx):
-    return next((i for i in range(idx, len(arr)) if arr[i] == 1), -1)
+    for i in range(idx, len(arr)):
+        if arr[i] == 1:
+            return i
+    return -1

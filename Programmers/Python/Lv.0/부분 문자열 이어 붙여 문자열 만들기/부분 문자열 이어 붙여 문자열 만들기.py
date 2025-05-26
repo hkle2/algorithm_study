88b018687f0df5ebef1649b69bb0d66d343be8d9
hklee <hklee2598@gmail.com>
2025-05-26 21:50:01 +0900

    Time: 0.02 ms, Memory: 9.4 MB

--- Programmers/Python/Lv.0/부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.py
+++ Programmers/Python/Lv.0/부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.py
@@ -1,5 +1,8 @@
+# def solution(my_strings, parts):
+#     answer = ""
+#     for string, (s, e) in zip(my_strings, parts):
+#         answer += string[s:e+1]
+#     return answer
+
 def solution(my_strings, parts):
-    answer = ""
-    for string, (s, e) in zip(my_strings, parts):
-        answer += string[s:e+1]
-    return answer
+    return "".join(string[s:e+1] for string, (s, e) in zip(my_strings, parts))

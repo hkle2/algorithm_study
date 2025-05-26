347a157a1d9bf22ca2ac1065b655579843a42a01
hklee <hklee2598@gmail.com>
2025-05-26 22:18:16 +0900

    Time: 0.00 ms, Memory: 9.2 MB

---
+++ Programmers/Python/Lv.0/마지막 두 원소/마지막 두 원소.py
@@ -0,0 +1,7 @@
+def solution(num_list):
+    diff_num = num_list[-1] - num_list[-2]
+    if diff_num > 0:
+        num_list.append(diff_num)
+    else:
+        num_list.append(num_list[-1] * 2)
+    return num_list

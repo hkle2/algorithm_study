f17555a4dca39589a0030b8d4a9bae633fcb207e
hklee <hklee2598@gmail.com>
2025-05-27 22:46:55 +0900

    Time: 10.75 ms, Memory: 12.5 MB

--- Programmers/Python/Lv.0/수 조작하기 2/수 조작하기 2.py
+++ Programmers/Python/Lv.0/수 조작하기 2/수 조작하기 2.py
@@ -1,4 +1,4 @@
 def solution(numLog):
-    num_dict = {1: "w", -1: "s", 10: "d", -10: "a"}
-    return "".join(num_dict[numLog[i+1] - numLog[i]] for i in range(len(numLog)-1))
+    move = {1: "w", -1: "s", 10: "d", -10: "a"}
+    return "".join(move[numLog[i+1] - numLog[i]] for i in range(len(numLog)-1))
         

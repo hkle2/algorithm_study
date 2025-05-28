5953aec82ee94f472b23fee8389e33892e724a1e
hklee <hklee2598@gmail.com>
2025-05-28 12:40:57 +0900

    Time: 3.17 ms, Memory: 15.7 MB

--- Programmers/Python/Lv.0/조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.py
+++ Programmers/Python/Lv.0/조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.py
@@ -1,5 +1,8 @@
+# def solution(arr, k):
+#     if k % 2 != 0:
+#         return [a * k for a in arr]
+#     else:
+#         return [a + k for a in arr]
+
 def solution(arr, k):
-    if k % 2 != 0:
-        return [a * k for a in arr]
-    else:
-        return [a + k for a in arr]
+    return [a * k for a in arr] if k % 2 != 0 else [a + k for a in arr]

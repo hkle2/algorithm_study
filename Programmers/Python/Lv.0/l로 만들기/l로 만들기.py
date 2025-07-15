ef06b3c7a55cf78953538933c19eeec5e4b162d1
hklee <hklee2598@gmail.com>
2025-07-15 18:08:51 +0900

    Time: 7.16 ms, Memory: 9.77 MB

--- Programmers/Python/Lv.0/l로 만들기/l로 만들기.py
+++ Programmers/Python/Lv.0/l로 만들기/l로 만들기.py
@@ -1,5 +1,8 @@
+# def solution(myString):
+#     for c in myString:
+#         if c < "l":
+#             myString = myString.replace(c, "l")
+#     return myString
+
 def solution(myString):
-    for c in myString:
-        if c < "l":
-            myString = myString.replace(c, "l")
-    return myString
+    return "".join("l" if c < "l" else c for c in myString)

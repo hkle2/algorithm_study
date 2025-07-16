74a81e0960de69d92e15360acb61faa647f696bf
hklee <hklee2598@gmail.com>
2025-07-16 18:21:54 +0900

    Time: 0.00 ms, Memory: 9.04 MB

--- Programmers/Python/Lv.0/주사위 게임 1/주사위 게임 1.py
+++ Programmers/Python/Lv.0/주사위 게임 1/주사위 게임 1.py
@@ -1,7 +1,10 @@
+# def solution(a, b):
+#     if (a + b) % 2 == 1:
+#         return 2 * (a + b)
+#     elif a % 2 == 1:
+#         return a**2 + b**2
+#     else:
+#         return abs(a - b)
+
 def solution(a, b):
-    if (a + b) % 2 == 1:
-        return 2 * (a + b)
-    elif a % 2 == 1:
-        return a**2 + b**2
-    else:
-        return abs(a - b)
+    return 2 * (a + b) if (a + b) % 2 == 1 else (a**2 + b**2 if a % 2 == 1 else abs(a - b))

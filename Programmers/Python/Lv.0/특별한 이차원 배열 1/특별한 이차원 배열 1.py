301f19f9e9f85cef3a152f45c6c067fcb67110f9
hklee <hklee2598@gmail.com>
2025-07-16 22:32:11 +0900

    Time: 0.27 ms, Memory: 9.5 MB

--- Programmers/Python/Lv.0/특별한 이차원 배열 1/특별한 이차원 배열 1.py
+++ Programmers/Python/Lv.0/특별한 이차원 배열 1/특별한 이차원 배열 1.py
@@ -1,7 +1,10 @@
+# def solution(n):
+#     arr = [[0] * n for _ in range(n)]
+#     for i in range(n):
+#         for j in range(n):
+#             if i == j:
+#                 arr[i][j] = 1
+#     return arr
+
 def solution(n):
-    arr = [[0] * n for _ in range(n)]
-    for i in range(n):
-        for j in range(n):
-            if i == j:
-                arr[i][j] = 1
-    return arr
+    return [[1 if i == j else 0 for j in range(n)] for i in range(n)]

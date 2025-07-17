3457691ff0322245f96d534bf6cf8f63ed2b438a
hklee <hklee2598@gmail.com>
2025-07-17 21:21:23 +0900

    Time: 0.01 ms, Memory: 9.13 MB

--- Programmers/Python/Lv.0/피자 나눠 먹기 (2)/피자 나눠 먹기 （2）.py
+++ Programmers/Python/Lv.0/피자 나눠 먹기 (2)/피자 나눠 먹기 （2）.py
@@ -1,7 +1,10 @@
+# def solution(n):
+#     i = 1
+#     while True:
+#         if 6 * i % n == 0:
+#             return i
+#         else:
+#             i += 1
+
 def solution(n):
-    i = 1
-    while True:
-        if 6 * i % n == 0:
-            return i
-        else:
-            i += 1
+    return next(i for i in range(1, n+1) if 6 * i % n == 0)

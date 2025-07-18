835321401e74a98a77085517f54b9503941ebbcc
hklee <hklee2598@gmail.com>
2025-07-18 14:35:08 +0900

    Time: 0.00 ms, Memory: 9.13 MB

--- Programmers/Python/Lv.0/중복된 문자 제거/중복된 문자 제거.py
+++ Programmers/Python/Lv.0/중복된 문자 제거/중복된 문자 제거.py
@@ -1,6 +1,9 @@
+# def solution(my_string):
+#     answer = []
+#     for str in my_string:
+#         if str not in answer:
+#             answer.append(str)
+#     return "".join(answer)
+
 def solution(my_string):
-    answer = []
-    for str in my_string:
-        if str not in answer:
-            answer.append(str)
-    return "".join(answer)
+    return "".join(dict.fromkeys(my_string))
